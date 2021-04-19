887ba04f38f7a8a52e3dcd590b4c3b81bf024146
Zockedidock <joona.brueckner@gmail.com>
2021-04-19 15:20:03 +0200

    tried implementing the snake system...

--- GameCanvas.java
+++ GameCanvas.java
@@ -20,7 +20,9 @@ public class GameCanvas extends JPanel implements KeyListener
 {
   Apple ppl;
   ArrayList<Integer> x = new ArrayList<Integer>();
-  ArrayList<Integer> y = new ArrayList<Integer>();    
+  ArrayList<Integer> y = new ArrayList<Integer>(); 
+  int xTEMP;
+  int yTEMP;   
   String direction = "R";
   Timer time = new Timer();
   static Wini ini;
@@ -53,11 +55,15 @@ public class GameCanvas extends JPanel implements KeyListener
     System.out.println(unit);
     g.setColor(Color.green);
     g.fillRect(0, 0, 700, 550);
-  
-    //for (int i = 0; i <= (snake_lenght-1); i++) {
-      g.setColor(Color.black);
+    
+    if (x.size() != 0) {
       g.fillRect(x.get(0), y.get(0), this.unit, this.unit);
-    //} 
+      for (int i = 0; i <= (x.size()-1); i++) {
+        g.setColor(Color.black);
+        g.fillRect(x.get(i), y.get(i), this.unit, this.unit);
+      } 
+    }
+    
   
     g.setColor(Color.red);
     g.fillRect(ppl.getX(), ppl.getY(), this.unit, this.unit);     
@@ -68,27 +74,79 @@ public class GameCanvas extends JPanel implements KeyListener
         @Override
         public void run()
         {
-          //for (int i = 0;i <= (snake_lenght-1);i++) {
-            x.set(0, x.get(0));
-            y.set(0, y.get(0));
-          //} // end of for
           System.out.println(unit);
+                               
+          xTEMP = x.get(0);
+          yTEMP = y.get(0);
+            
           switch (direction) {
             case "R" : 
-              x.set(0, (x.get(0) + unit));
+              for (int i = 0; i <= (x.size()-1); i++) {
+                g.setColor(Color.black);
+                g.fillRect(x.get(i), y.get(i), this.unit, this.unit);
+              } 
+              x.set(x.size()-1, (x.get(x.size()-1) + unit));
               break;
             case "L" : 
-              x.set(0, (x.get(0) - unit));
+              x.set(x.size()-1, (x.get(x.size()-1) - unit));
               break;
             case "U" : 
-              y.set(0, (y.get(0) + unit));
+              y.set(x.size()-1, (y.get(x.size()-1) + unit));
               break;
             case "D" : 
-              y.set(0, (y.get(0) - unit));
+              y.set(x.size()-1, (y.get(x.size()-1) - unit));
               break;
-            default: 
-              
+            default:  
           } // end of switch
+          
+          if ((x.get(0) == ppl.getX()) && (y.get(0) == ppl.getY())) {
+            //System.out.println("test222222");
+            switch (direction) {
+              case "R" : 
+                x.add(ppl.getX() + unit);
+                y.add(y.get(y.size()-1));
+                if (x.size() > 2) {
+                  x.remove(0);
+                  y.remove(0);  
+                } // end of if
+                break;
+              case "L" : 
+                x.add(ppl.getX() - unit);
+                y.add(y.get(y.size()-1));
+                if (x.size() > 2) {
+                  x.remove(0);
+                  y.remove(0);  
+                } // end of if
+                break;
+              case "U" : 
+                x.add(x.get(x.size()-1));
+                y.add(ppl.getY() + unit);
+                if (x.size() > 2) {
+                  x.remove(0);
+                  y.remove(0);  
+                } // end of if
+                break;
+              case "D" : 
+                x.add(x.get(x.size()-1));
+                y.add(ppl.getY() - unit);
+                if (x.size() > 2) {
+                  x.remove(0);
+                  y.remove(0);  
+                } // end of if
+                break;
+              default:  
+            } // end of switch
+          }
+          /*x.add(xTEMP);
+          y.add(yTEMP);
+          x.remove(x.size()-1);
+          y.remove(y.size()-1);
+          
+          for (int i = 1;i <= (x.size()-1); i++) {
+            x.set(i, x.get(i+1));
+            x.set(i, x.get(i+1));
+          } // end of for
+            */
           repaint();
         }
       }, 800);
